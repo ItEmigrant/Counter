aedd5101005e261c9156143f322ff9c822929a1a
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-03-11 10:47:15 +0100

    CounterUniversalAlmostTheEnd

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,18 @@ function App() {
     let [maxValue, setMaxValue] = useState(5);
     let [minValue, setMinValue] = useState(0);
 
-    let [max, setMax] = useState();
-    let [min, setMin] = useState();
 
     let [count, setCount] = useState(minValue);
 
-  const updateState =()=> {
 
-  }
 
     return (
         <div className="App">
             <Counter maxValue={maxValue} minValue={minValue}
                      count={count} setCount={setCount}/>
             <br/>
-            <SettingsCounter updateState={ updateState} />
+            <SettingsCounter setMinValue={setMinValue}  minValue={minValue}
+                             setMaxValue={setMaxValue}  maxValue={maxValue}/>
 
         </div>
     );
--- src/Components/Counter/settingsCounter.tsx
+++ src/Components/Counter/settingsCounter.tsx
@@ -3,32 +3,43 @@ import s from "./SettingsCounter.module.css";
 import {Button} from "./UniversalButton";
 
 type SettingsCounterPropsType = {
-    updateState:(newValue:string)=> void
+    setMinValue: (newValue: number) => void
+    minValue: number
+
+    setMaxValue: (newValue: number) => void
+    maxValue: number
 }
 
-export const SettingsCounter = (props:SettingsCounterPropsType) => {
+export const SettingsCounter = (props: SettingsCounterPropsType) => {
 
-const updateMinValue = (e: ChangeEvent<HTMLInputElement>) => {
-  props.updateState(e.currentTarget.value)
+    const updateMinValue = (e: ChangeEvent<HTMLInputElement>) => {
+        props.setMinValue(Number(e.currentTarget.value))
     }
 
+    const updateMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+        props.setMaxValue(Number(e.currentTarget.value))
+    }
+
+    const onClickResetHandler = () => {
+        props.setMaxValue(props.maxValue)
+    }
 
     return (
         <div className={s.body}> SETTINGS
             <div className={s.count}>
                 <h4>
-                    <div>max value: <input type="number" onChange={updateMinValue}/></div>
+                    <div>max value: <input type="number" onChange={updateMaxValue}/></div>
                 </h4>
 
                 <h4>
-                    <div>min value: <input type="number"/></div>
+                    <div>min value: <input type="number" onChange={updateMinValue}/></div>
                 </h4>
             </div>
             - settings for counter-
             <div className={s.button}>
                 <div>
 
-                    <Button name={'SETTINGS'} callBack={ ()=>{}} disabled={false}/>
+                    <Button name={'SETTINGS'} callBack={onClickResetHandler} disabled={false}/>
                 </div>
 
             </div>
